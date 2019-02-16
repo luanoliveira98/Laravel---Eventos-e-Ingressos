83f03a7fbd91451372b2d835735bc585d7f96a60
Luan <luanoliveira17@hotmail.com>
2019-02-16 15:43:47 -0200

    Users Migration (Seeder)
    Login

---
+++ site/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrador',
+            'email' => 'admin@4all.com',
+            'password' => bcrypt('admin123'),
+            'admin' => 'S'
+        ]);
+    }
+}
